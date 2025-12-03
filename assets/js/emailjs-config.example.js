9691916cd90932c6076690c3c8f8b6f61b72bb5c
otocomdev <otocomdev@gmail.com>
2025-12-03 15:14:54 +0300

    update env

---
+++ assets/js/emailjs-config.example.js
@@ -0,0 +1,9 @@
+// EmailJS Konfigürasyonu - ÖRNEK DOSYA
+// Bu dosyayı kopyalayıp emailjs-config.js olarak kaydedin
+// ve kendi değerlerinizi girin
+
+const EMAILJS_CONFIG = {
+    publicKey: 'YOUR_PUBLIC_KEY_HERE',
+    serviceId: 'YOUR_SERVICE_ID_HERE',
+    templateId: 'YOUR_TEMPLATE_ID_HERE'
+};
